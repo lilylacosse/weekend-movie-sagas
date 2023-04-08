d530cb4c6e5c1d502004eacf1f162b235fa15512
Lily LaCosse <lilylacosse@gmail.com>
2023-04-08 17:45:05 -0500

    updated name of AddMovie component, did some code formatting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import MovieList from "../MovieList/MovieList";
 import Details from "../Details/Details";
-import Movie from "../Movie/Movie";
+import AddMovie from "../AddMovie/AddMovie";
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
         <Route path="/details">
           <Details />
         </Route>
-        <Route path="/movie">
-          <Movie />
+        <Route path="/addmovie">
+          <AddMovie />
         </Route>
       </Router>
     </div>
